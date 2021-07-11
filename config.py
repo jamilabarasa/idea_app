78d125eefb9a65a82ef8114c7b5f613ebce7a85c
“jamilabarasa” <jamilabarasa18@gmail.com>
2021-07-11 19:51:57 +0300

    setup environment

---
+++ config.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+db_path=os.path.join(os.path.dirname(__file__),'app.db')
+
+db_uri = 'sqlite:///{}'.format(db_path)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
