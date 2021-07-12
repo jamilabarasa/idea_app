83e3d593fa4b8abd6f3c0179f13cf06963e72d38
brian <briankathukya2000@gmail.com>
2021-07-12 14:23:53 +0300

    Added logic to save team

--- repository.py
+++ repository.py
@@ -1,4 +1,4 @@
-from models import db, Member
+from models import db, Member, Team
 
 def saveMember(member:Member):
     
@@ -17,5 +17,18 @@ def deleteMemberById(id:int):
     member:Member = getMemberById(id)
 
     db.session.delete(member)
+
+    db.session.commit()
+
+
+def findMemberByEmail(email:str):
+
+    member = Member.query.filter_by(email=email).first()
+
+    return member
+
+def saveTeam(team:Team):
+
+    db.session.add(team)
 
     db.session.commit()
